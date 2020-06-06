1bb994419448813631b3ca5c1394b0ca825770d8
Kibet Masai <humphreykibet@gmail.com>
2020-06-06 08:01:07 +0300

    updated get_number_peaks.py hkm

---
+++ Social Networks/find_eulerian_tour.py
@@ -0,0 +1,17 @@
+# Find Eulerian Tour
+#
+# Write a function that takes in a graph
+# represented as a list of tuples
+# and return a list of nodes that
+# you would follow on an Eulerian Tour
+#
+# For example, if the input graph was
+# [(1, 2), (2, 3), (3, 1)]
+# A possible Eulerian tour would be [1, 2, 3, 1]
+
+def find_eulerian_tour(graph):
+    # your code here
+    return []
+
+
+
