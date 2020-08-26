06f81d4f83e5a723f40986008fc1a781cadaeb60
Kibet Masai <humphreykibet@gmail.com>
2020-08-26 06:57:42 +0300

    Hashing/HashTable.py hkm

---
+++ DSA/Maps & Hashing/HashTable.py
@@ -0,0 +1,52 @@
+class HashTable(object):
+    def __init__(self):
+        self.table = [None]*10000
+
+    def store(self, string):
+        """Input a string that's stored in
+             the table."""
+        hv = self.calculate_hash_value(string)
+        if hv != -1:
+            if self.table[hv] != None:
+                self.table[hv].append(string)
+            else:
+                self.table[hv] = [string]
+
+    def lookup(self, string):
+        """Return the hash value if the
+        string is already in the table.
+        Return -1 otherwise."""
+        hv = self.calculate_hash_value(string)
+        if hv != -1:
+            if self.table[hv] != None:
+                if string in self.table[hv]:
+                    return hv
+        return -1
+
+    def calculate_hash_value(self, string):
+        """Helper function to calulate a
+                hash value from a string."""
+        value = ord(string[0])*100 + ord(string[1])
+        return value
+
+
+# Setup
+hash_table = HashTable()
+
+# Test calculate_hash_value
+# Should be 8568
+print(hash_table.calculate_hash_value('UDACITY'))
+
+# Test lookup edge case
+# Should be -1
+print(hash_table.lookup('UDACITY'))
+
+# Test store
+hash_table.store('UDACITY')
+# Should be 8568
+print(hash_table.lookup('UDACITY'))
+
+# Test store edge case
+hash_table.store('UDACIOUS')
+# Should be 8568
+print(hash_table.lookup('UDACIOUS'))
