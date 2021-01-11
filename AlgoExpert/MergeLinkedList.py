e41b4b929c8c5dd09f2c3657839cd0adf192a03a
Kibet Masai <humphreykibet@gmail.com>
2021-01-11 05:58:29 +0300

    updated merge linked lists, remove nth node from end, and reverse linked list hkm

---
+++ AlgoExpert/MergeLinkedList.py
@@ -0,0 +1,45 @@
+class LinkedList:
+    def __init__(self,value):
+        self.value =value
+        self.next =None
+
+    # O(n + m) time | O(1) space
+    def mergeLinkedLists(self, headOne, headTwo):
+        p1 =headOne
+        p1Prev =None
+        p2 =headTwo
+
+        while p1 is not None and p2 is not None:
+            if p1.value < p2.value:
+                p1Prev =p1
+                p = p1.next
+            else:
+                if p1Prev is not None:
+                    p1Prev.next = p2
+                p1Prev =p2
+                p2 =p2.next
+                p1Prev.next = p1
+        if p1 is None:
+            p1Prev.next =p2
+        return headOne if headOne.value < headTwo.value else headTwo
+
+    # Recursive solution
+    # O(n + m) time | O(n + m) space
+    def mergeLinkedListsRecursive(self,headOne, headTwo):
+        self.recursiveMerge(headOne, headTwo, None)
+        return headOne if headOne.value < headTwo.value else headTwo
+
+    def recursiveMerge(self,p1, p2, p1Prev):
+        if p1 is None:
+            p1Prev.next =p2
+            return
+        if p2 is None:
+            return
+        if p1.value < p2.value:
+            self.recursiveMerge(p1.next, p2, p1)
+        else:
+            if p1Prev is not None:
+                p1Prev.next =p2
+            newP2 =p2.next
+            p2.next =p1
+            self.recursiveMerge(p1,newP2,p2)
---
+++ AlgoExpert/RemoveNthNodeFromEnd.py
@@ -0,0 +1,16 @@
+# O(n) time | O(1) space
+def removeNthNodeFromEnd(head, n):
+    counter =1
+    first =head
+    second =head
+    while counter <= n:
+        second = second.next
+        counter += 1
+    if second is None:
+        head.value =head.next.value
+        head.next =head.next.next
+    while second.next is not None:
+        second =second.next
+        first =first.next
+    first.next = first.next.next
+
