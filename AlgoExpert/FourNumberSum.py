fbba435ccfce0a6d33e6d46c6cfbd656b52733d4
Kibet Masai <humphreykibet@gmail.com>
2021-01-26 05:05:10 +0300

    added FourNumberSum, subarraySort hkm

--- AlgoExpert/FourNumberSum.py
+++ AlgoExpert/FourNumberSum.py
@@ -40,10 +40,29 @@ def fourNumberSum1(array,targetSum):
             right1 +=1
     return []
 
+# O(n ^ 2 ) time | O(n ^ 2) space
+def fourNumberSum2(array, targetSum):
+    allPairSums ={}
+    quadruplets =[]
+    for i in range(1, len(array) - 1):
+        for j in range(i +1, len(array)):
+            currentSum = array[i] + array[j]
+            difference = targetSum - currentSum
+            if difference in allPairSums:
+                for pair in allPairSums[difference]:
+                    quadruplets.append(pair + [array[i], array[j]])
+        for k in range(0,i):
+            currentSum =array[i] +array[k]
+            if currentSum not in allPairSums:
+                allPairSums[currentSum] =[[array[k], array[i]]]
+            else:
+                allPairSums[currentSum].append([array[k],array[i]])
+    return quadruplets
 
 
+# my_array = [7, 6, 4, -1, 1, 2]
 my_array = [7, 6, 4, -1, 1, 2]
 sum = 16
-print(fourNumberSum(my_array, sum))
+print(fourNumberSum2(my_array, sum))
 
 # [1, 2, 7, 6], [7, 6, 4, -1]
