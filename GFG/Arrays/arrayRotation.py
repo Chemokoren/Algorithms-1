4bba90b3ec8d925238c30d9640e6b6dd81b3381b
kibet masai <humphreykibet@gmail.com>
2021-05-31 05:00:01 +0300

    updated GFG/Arrays/arrayRotation.py hkm

--- GFG/Arrays/arrayRotation.py
+++ GFG/Arrays/arrayRotation.py
@@ -98,3 +98,67 @@ leftRotate(arr, d, n)
 
 print("after rotation \n")
 printArray(arr, n)
+
+
+print("\n################################## Method 4 ##################################")
+
+"""
+Method 4 (The Reversal Algorithm) :
+rotate(arr[], d, n)
+  reverse(arr[], 1, d) ;
+  reverse(arr[], d + 1, n);
+  reverse(arr[], 1, n);
+
+Let AB are the two parts of the input array where A = arr[0..d-1] and B = arr[d..n-1]. The idea of the algorithm is : 
+ 
+
+Reverse A to get ArB, where Ar is reverse of A.
+Reverse B to get ArBr, where Br is reverse of B.
+Reverse all to get (ArBr) r = BA.
+Example : 
+Let the array be arr[] = [1, 2, 3, 4, 5, 6, 7], d =2 and n = 7 
+A = [1, 2] and B = [3, 4, 5, 6, 7] 
+ 
+
+Reverse A, we get ArB = [2, 1, 3, 4, 5, 6, 7]
+Reverse B, we get ArBr = [2, 1, 7, 6, 5, 4, 3]
+Reverse all, we get (ArBr)r = [3, 4, 5, 6, 7, 1, 2]
+
+Time Complexity : O(n)
+
+"""
+
+# program for reversal algorithm of array rotation
+# function to reverse arr[] from index start to end
+
+def reverseArray(arr, start, end):
+    while(start < end):
+        temp =arr[start]
+        arr[start] = arr[end]
+        arr[end] =temp
+        start += 1
+        end =end - 1
+
+# Function to left rotate arr[] of size n by d
+def leftRotate(arr, d):
+    if d == 0:
+        return
+    n = len(arr)
+    # in case the rotating factor is greater than array length
+    d = d % n
+    reverseArray(arr, 0, d-1)
+    reverseArray(arr, d, n-1)
+    reverseArray(arr, 0, n-1)
+
+# Function to print an array
+def printArray4(arr):
+    for i in range(0,len(arr)):
+        print(arr[i],end=" ")
+
+# test above functions
+arr =[1,2,3,4,5,6,7]
+n = len(arr)
+d = 2
+
+leftRotate(arr,d) # Rotate array by 2
+printArray4(arr)
