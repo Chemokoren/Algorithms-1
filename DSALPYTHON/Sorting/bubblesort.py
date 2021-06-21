a5d57d3411cba6e9587e0b9eb5fb5288286702d4
kibet masai <humphreykibet@gmail.com>
2021-06-21 06:54:03 +0300

    DSALPYTHON/Sorting/ hkm

---
+++ DSALPYTHON/Sorting/bubblesort.py
@@ -0,0 +1,11 @@
+def bubblesort(A):
+    
+    for i in range(len(A) -1, 0, -1):
+        for j in range(i):
+            if A[j] > A[j+1]:
+                A[j], A[j+1] = A[j+1], A[j]
+
+A =[84, 21, 96, 15, 47]
+print("Original Array: ", A)
+bubblesort(A)
+print('Sorted Array: ', A)
