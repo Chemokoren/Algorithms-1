a8d82a45c001d3d1810ef7dba0de577ffb54d494
Masai Kibet <humphreykibet@gmail.com>
2021-09-16 16:28:00 +0300

    updated longes substring hkm

--- leedcode/arrays_strings_questions/longest-substring-without-repeating-characters.py
+++ leedcode/arrays_strings_questions/longest-substring-without-repeating-characters.py
@@ -32,3 +32,21 @@ Constraints:
 0 <= s.length <= 5 * 104
 s consists of English letters, digits, symbols and spaces.
 """
+
+class Solution:
+
+    def lengthOfLongestSubstring(self, s:str) -> int:
+        m = {}
+        left = 0
+        right = 0
+        ans = 0
+        n = len(s)
+
+        while(left < n and right< n):
+            el = s[right]
+            if(el in m):
+                left = max(left, m[el] + 1)
+            m[el] = right
+            ans = max(ans, right - left + 1)
+            right += 1
+        return ans
