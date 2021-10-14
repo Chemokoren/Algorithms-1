b9fe5fc06445193a00034209f955f0e64543a324
kibsoft <humphreykibet@gmail.com>
2021-10-14 20:57:44 +0300

    same_bsts, zigzag_traverse hkm

---
+++ AlgoExpert/AlgoEx/45_5_zigzag_traverse.py
@@ -0,0 +1,48 @@
+"""
+            1   3   4   10
+
+            2   5   9   11
+            
+            6   8   12  15
+
+            7   13  14  16
+
+Output =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+
+"""
+
+# O(n) time | O(n) space
+def zigzagTraverse(array):
+    height =len(array) -1
+    width = len(array[0]) -1
+    result = []
+    row, col = 0,0
+    goingDown = True
+    while not isOutOfBounds(row, col, height, width):
+        result.append(array[row][col])
+        if goingDown:
+            if col == 0 or row == height:
+                goingDown = False
+                if row == height:
+                    col +=1
+                else:
+                    row +=1
+            else:
+                row += 1
+                col -= 1
+        else:
+            if row == 0 or col == width:
+                goingDown = True
+                if col == width:
+                    row += 1
+                else:
+                    col +=1
+    return result
+
+def isOutOfBounds(row, col, height, width):
+    return row < 0 or row > height or col < 0 or col > width
+
+#my_array=[[1,3,4,10],[2,5,9,11],[6,8,12,15],[7,13,14,16]]
+my_array=[1,3,4,10,2,5,9,11,6,8,12,15,7,13,14,16]
+
+print(zigzagTraverse(my_array))
