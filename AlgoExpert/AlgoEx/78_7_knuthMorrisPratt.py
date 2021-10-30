584fffe193db682a9c411fda9ddebcd33f12c8c8
kibsoft <humphreykibet@gmail.com>
2021-10-30 07:04:55 +0300

    added rectangle mania hkm

---
+++ AlgoExpert/AlgoEx/78_7_knuthMorrisPratt.py
@@ -0,0 +1,44 @@
+"""
+Knuth Morris Pratt
+"""
+
+# O(n+m) time | O(m) space
+def knuthMorrisPrattAlgorithm(string, substring):
+    pattern = buildPattern(substring)
+    return doesMatch(string, substring, pattern)
+
+def buildPattern(substring):
+    pattern =[-1 for i in substring]
+    j = 0
+    i = 1
+    while i < len(substring):
+        if substring[i] == substring[j]:
+            pattern[i] = j
+            i += 1
+            j += 1
+        elif j > 0:
+            j = pattern[j - 1] + 1
+        else:
+            i += 1
+    return pattern
+
+def doesMatch(string, substring, pattern):
+    i = 0
+    j = 0
+    while i + len(substring) - j <= len(string):
+        if string[i] == substring[j]:
+            if j == len(substring) - 1:
+                return True
+            i += 1
+            j += 1
+        elif j > 0:
+            j = pattern[j - 1] + 1
+        else:
+            i += 1
+    return False
+
+my_string ="aefaefaefaedaefaedaefaefa"
+my_sub_string="aefaedaefaefa"
+
+print(knuthMorrisPrattAlgorithm(my_string,my_sub_string))
+
