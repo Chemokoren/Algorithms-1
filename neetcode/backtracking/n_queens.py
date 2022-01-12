a521058ca997698240b83d17e7a93bc80e2aaf6d
kibsoft <humphreykibet@gmail.com>
2022-01-12 06:42:36 +0300

    n queens hkm

---
+++ neetcode/backtracking/n_queens.py
@@ -0,0 +1,61 @@
+"""
+The n-queens puzle is the problem of placing n queens on an n*n chessboard such that no two
+queens attack each other.
+
+Given an integer n, return all distinct solutions to the n-queens puzzle. You may return the 
+answer in any order. Each solution contains a distinct board configuration of the n-queens'
+placement, where 'Q' and '.' both indicate a queen and an empty space respectively.
+Example 1:
+
+.       Q       .       .               .       .       Q       .
+
+.       .       .       Q               Q       .       .       .
+
+Q       .       .       .               .       .       .       Q
+
+.       .       Q       .               .       Q       .       .
+
+Input n = 4
+
+Output: [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above
+"""
+from typing import List
+class Solution:
+
+    def SolveNQueens(self, n: int)->List[List[str]]:
+        col = set()
+        posDiag =set() # (r + c)
+        negDiag =set() # (r - c)
+
+        res =[]
+        board =[["."] * n for i in range(n)]
+
+        def backtrack(r):
+            if r == n:
+                copy =["".join(row) for row in board]
+                res.append(copy)
+                return
+
+            for c in range(n):
+                if c in col or (r + c) in posDiag or (r - c) in negDiag:
+                    continue
+
+                col.add(c)
+                posDiag.add(r + c)
+                negDiag.add(r - c)
+                board[r][c] = "Q"
+
+                backtrack(r + 1)
+
+                col.remove(c)
+                posDiag.remove(r + c)
+                negDiag.remove(r - c)
+                board[r][c] ="."
+
+        backtrack(0)
+        return res
+
+n = 4
+sol = Solution()
+print(sol.SolveNQueens(n))
---
+++ neetcode/backtracking/word_search_ii.py
@@ -0,0 +1,70 @@
+from typing import List
+"""
+Given an m*x board of characters and a list of strings words, return all words on the board.
+Each word must be constructed from letters of sequentially adjacent cells, where adjacent cells
+are horizontally or vertically neighboring. The same letter cell may not be used more than
+once in a word.
+
+Example 1:
+
+o       a       a       n
+
+e       t       a       e
+
+i       h       k       r
+
+i       f       l       v
+
+Input: board =[["o","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]],
+words =["oath","eat","rain"]
+Output: ["eat","oath"]
+"""
+
+class TrieNode:
+
+    def __init__(self):
+        self.children ={}
+        self.isWord = False
+
+    def addWord(self, word):
+        cur = self
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] =TrieNode()
+            cur =cur.children[c]
+        cur.isWord =True
+
+class Solution:
+
+    def findWords(self, board: List[List[str]], words: List[str])->List[str]:
+        root = TrieNode()
+        for w in words:
+            root.addWord(w)
+
+        ROWS, COLS = len(board), len(board[0])
+        res, visit =set(), set()
+
+        def dfs(r, c, node, word):
+            if(r < 0 or c < 0 or r ==ROWS or c == COLS or board[r][c] not in node.children or(r,c) in visit):
+                return
+            visit.add((r, c))
+            node = node.children[board[r][c]]
+            word += board[r][c]
+            if node.isWord:
+                res.add(word)
+
+            dfs(r + 1, c , node, word)
+            dfs(r - 1, c , node, word)
+            dfs(r , c + 1, node, word)
+            dfs(r , c - 1, node, word)
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                dfs(r,c, root, "")
+        return list(res)
+
+board =[["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]]
+words =["oath","pea","eat","rain"]
+
+sol =Solution()
+print(sol.findWords(board,words))
